f785fe35dfa2d9c420af60cd04e6d5f0067ae276
fatisand <fatiisandoval2000@gmail.com>
2024-04-26 18:59:31 +0200

    agg

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -11,6 +11,7 @@ class IndexController extends AbstractController
     #[Route('/', name: 'app_index')]
     public function index(): Response
     {
+        $this->denyAccessUnlessGranted('ROLE_USER');
         return $this->render('index/index.html.twig', [
             'controller_name' => 'IndexController',
         ]);
